3f948209a06d7988ae35bc32e6c8d82e455a0109
Briyan Adityatama <briyansykes@yahoo.com>
2018-11-26 23:32:52 +0700

    add SendNotifications

---
+++ src/app/Notifications/SendVerificationCodeEmail.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace briyanadityatama\laravelphp2step\App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class SendVerificationCodeEmail extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $code;
+    protected $user;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($user, $code)
+    {
+        $this->code = $code;
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $message = new MailMessage();
+        $message
+            ->from(config('laravelphp2step.verificationEmailFrom'), config('laravelphp2step.verificationEmailFromName'))
+            ->subject(trans('laravelphp2step::laravel-verification.verificationEmailSubject'))
+            ->greeting(trans('laravelphp2step::laravel-verification.verificationEmailGreeting', ['username' => $this->user->name]))
+            ->line(trans('laravelphp2step::laravel-verification.verificationEmailMessage'))
+            ->line($this->code)
+            ->action(trans('laravelphp2step::laravel-verification.verificationEmailButton'), route('laravelphp2step::verificationNeeded'));
+
+        return $message;
+    }
+}
